d897c0b0c46af4d4813cf11314999620da35db94
vishaag <vishaag@gmail.com>
2018-10-17 14:41:59 +0530

    Add home page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,6 +4,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Login | Beautiful Chat"
   end
 
 end
