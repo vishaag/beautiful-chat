9eaa236c238330b60a9058ab152469eef797cb6e
vishaag <vishaag@gmail.com>
2018-10-17 15:32:25 +0530

    Add User model

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,9 +2,9 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
-    assert_select "title", "Login | Beautiful Chat"
+    assert_select "title", "Welcome | Beautiful Chat"
   end
 
 end
