0f8d28fb98b48a960f277fd8a0c95bd596f0f5db
vishaag <vishaag@gmail.com>
2018-10-18 11:55:13 +0530

    Add remember me feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,12 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       log_in user
+      if params[:session][:remember_me] == '1'
+        remember user
+      else
+        forget user
+      end
+      remember user
       redirect_to user
     else
       flash[:danger] = 'Invalid email/password combination'
@@ -14,7 +20,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 
