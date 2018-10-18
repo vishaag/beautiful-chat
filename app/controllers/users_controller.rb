05d8aea71115407bf6259032093d4f36804f8a4c
vishaag <vishaag@gmail.com>
2018-10-18 12:38:46 +0530

    Add updating user details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,15 +9,28 @@ class UsersController < ApplicationController
   end
 
   def create
-  @user = User.new(user_params)
-  if @user.save
-    log_in @user
-    flash[:success] = "Account created Successfully!"
-    redirect_to @user
-  else
-    render 'new'
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Account created Successfully!"
+      redirect_to @user
+    else
+      render 'new'
+    end 
   end
-  
+
+  def edit
+      @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   private
