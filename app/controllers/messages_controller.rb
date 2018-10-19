792a9b325d1314487f3f7f2f688cac297ca24a33
vishaag <vishaag@gmail.com>
2018-10-19 11:07:28 +0530

    implement super basic chat system

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,17 @@
 class MessagesController < ApplicationController
   def index
-    @old_messages = Message.all
+    @old_messages_sent = Message.where("sender_id = ? AND receiver_id = ?", session[:user_id], params[:id])
+    @old_messages_received = Message.where("sender_id = ? AND receiver_id = ?", params[:id], session[:user_id])
+
+    @old_messages = (@old_messages_sent + @old_messages_received).sort{|a,b| a.created_at <=> b.created_at}
     @message = Message.new
+
   end
 
   def create
     @message = Message.new(message_params)
+    @message.sender_id = session[:user_id]
+    @message.receiver_id = params[:id]
     @message.save
   end
 
